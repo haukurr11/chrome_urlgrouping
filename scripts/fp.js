0d0b8a346bc38b47e846e2ac3090d96b328b8665
Haukur Rosinkranz <hauxir@gmail.com>
2013-06-09 00:28:29 +0000

    A skeleton for our extension

---
+++ scripts/fp.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function() {
+	$("body").html("\
+	       <p>This is only a skeleton for our extension!</p> \
+		   <p>It will use JQuery, Bootstrap and Chrome API</p> \
+		   ");
+});
