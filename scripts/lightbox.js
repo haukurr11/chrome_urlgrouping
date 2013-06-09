21b0fd0bca19e32e7e787318b6c85ca9a071182f
Bergvin <Bergvinorn@gmail.com>
2013-06-09 23:39:09 +0000

    css template og fl

---
+++ scripts/lightbox.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".opna").click( function() {
+		$("#light").css("display","block");
+		$("#fade").css("display","block");
+	});
+	$("#loka").click( function() {
+		$("#light").css("display","none");
+		$("#fade").css("display","none");
+	});	
+});
+
+
+	
