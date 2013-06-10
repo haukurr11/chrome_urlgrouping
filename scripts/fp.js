862ec33796623400a4c13e6b1b2f34be3b007d23
Haukur Rosinkranz <hauxir@gmail.com>
2013-06-10 03:13:42 +0000

    Template now communicates with API

--- scripts/fp.js
+++ scripts/fp.js
@@ -1,6 +1,68 @@
 ﻿function refreshLinks() {
+    function open(i) {
+         return function () {
+         openGroup(i);
+        }
+    }
+    function remove(i) {
+         return function () {
+            if (window.confirm("Are you sure you want to remove " + i) ) {
+             removeGroup(i);
+            }
+        }
+    }
+    function addlink(i) {
+         return function () {
+              var title = prompt("Title of link","");
+              var url = prompt("URL of link","http://");
+              if(title && url) {
+                  addLinkToGroup(i,title,url);
+              }
+        }
+    }
+
+    $('.btn-toolbar').empty();
     chrome.storage.sync.get('link_db', function(result){
-        //TODO
+      var link_db = result.link_db;
+     for(var name in link_db) {
+         var div = $('<div class="btn-group"></div>');
+         var grp_btn = $('<button class="btn-large btn-inverse">' + name + '</button>');
+         grp_btn.click( open(name));
+         var toggle = $('<button class="btn-large btn-inverse dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>');
+         var list = $('<ul class="dropdown-menu"></ul>');
+         for(var it in link_db[name]) {
+             var title = link_db[name][it].title;
+             var url = link_db[name][it].url;
+             var ahref = $('<a group="' + name + '" url="' + url + '" class="remove">' + title + '</a>');
+             var link = $('<li></li>');
+             var img =  $('<i class="icon-remove"></i>');
+             ahref.append(img);
+             link.append(ahref);
+             list.append(link);
+         };
+         list.append('<li class="divider"></li>');
+         var add_li = $('<li><a class="opna" href="#">Add Link</a></li>');
+         add_li.click(addlink(name));
+         list.append(add_li);
+         var remove_li =  $('<li><a class="opna" href="#">Remove</a></li>');
+         remove_li.click(remove(name));
+         list.append(remove_li);
+         div.append(grp_btn);
+         div.append(toggle);
+         div.append(list);
+         $('.btn-toolbar').append(div);
+
+        $(".remove").each( function() {
+                     $(this).click( function() {
+                        var address = $(this).attr("url");
+                        var group = $(this).attr("group");
+                        if(window.confirm("Are you sure you want to remove " + address))
+                           {
+                              removeLinkFromGroup(group,address);
+                           }
+                       });
+             });
+     }
     });
 }
 
@@ -115,4 +177,10 @@ function editLinkFromGroup(groupname,prevurl,title,url) {
 
 $(document).ready(function() {
     refreshLinks();
+    $("#new-workspace").click( function() {
+        var name  = prompt("Name of Workspace","");
+        if(name) {
+        createGroup(name);
+        }
+    });
 });
